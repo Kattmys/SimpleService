3a98a4ff30ceedcef9920f708bf75f32ecc14bc5
jackiboi307 <jack.josefsson@gmail.com>
2025-06-04 17:06:08 +0200

    fixed pipe not working after giving no input

--- communicate.sh
+++ communicate.sh
@@ -5,5 +5,11 @@ DIR=`realpath -s $0`
 DIR=`dirname $DIR`
 cd $DIR
 
+# Check if input is empty
+if [[ -z "$@" ]]; then
+    echo "No input. Use command 'help' for a list of commands."
+    exit
+fi
+
 echo "$@" > pipes/send
 cat pipes/receive
